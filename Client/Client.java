2fa300f86df00c053ff4e1fd068f2cfe8f9c9fc3
Taylor, Joshua-Butah <joshua-butah.taylor@haw-hamburg.de>
2023-11-04 09:44:34 +0100

    New Repo Test

---
+++ Client/Client.java
@@ -0,0 +1,37 @@
+package Client;
+
+//import Regestry.TicTacToeAService;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    /**
+     * ipaddress is used if the host and client are not in the same network.
+     */
+    private static final String ipaddress = "31.16.156.35";
+    private static final int port = 1099;
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("client started");
+
+            // Locate the registry with the server's IP address
+            // Get the reference of the exported object from the RMI registry
+
+
+            Registry registry = LocateRegistry.getRegistry("localhost", port);
+            //TicTacToeAService ticTacToeAService = (TicTacToeAService) registry.lookup("bindedstub");
+
+
+            // invoking the methode findGame just for test purpose
+            //ticTacToeAService.findGame("");
+
+
+            System.out.println("client ended");
+
+        } catch (Exception e) {
+            System.out.println("client side error: " + e);
+        }
+    }
+}
